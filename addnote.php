4040289cc2ca2be71eebfc9899bc7a6f2e3fd07b
Adalen Vladi <adalenvladi@gmail.com>
2017-07-24 11:30:30 +0200

    Update addnote.php
    delete comment

--- addnote.php
+++ addnote.php
@@ -1,6 +1,14 @@
 <?php
 include 'db_connect.php';
 
+if (isset($_GET['remove'])) {
+	
+	$remove=mysqli_query($con,"DELETE FROM note WHERE def='".$_POST['def']."'");
+	mysqli_close($con);
+	
+} else{
+
+
 $notetoadd=mysqli_escape_string($con,$_POST['notetoadd']);
 $admin= mysqli_escape_string($con,$_POST['admin']);
 $user_id=mysqli_escape_string($con,$_POST['user_id']);
@@ -9,11 +17,7 @@ $query = " INSERT INTO note ( id, admin, note) VALUES ('".$user_id."', '".$admin
 		$result = mysqli_query($con,$query) or die('Error: ' . mysqli_error($con));
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
-
-
-
-
 mysqli_close($con);
+}
 
-
-?>
+?> 
