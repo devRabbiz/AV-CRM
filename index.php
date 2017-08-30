891cc5fa8df91bbfdea60e63f9b90556f89d84a0
adalenv <adalenvladi@gmail.com>
2017-08-30 18:06:04 +0200

    rm unc files

--- index.php
+++ index.php
@@ -8,12 +8,15 @@
 
 <?php
 
-if (file_exists('install.php')) {
+//if (file_exists('install.php')) {
 	
-	header('Location: install.php');
-	}
+//	header('Location: install.php');
+	//}
 	
-
+//will removed soon
 ?>
+
+<a href="admin.php">Admin</a>
+<a href="operator.php">Operator</a>
 </body>
 </html>
