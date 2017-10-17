0124aa2602875b8ad5e60237cbf5016275217d8b
adalenv <adalenvladi@gmail.com>
2017-10-17 10:24:51 +0200

    remove status on move

--- move_sec.php
+++ move_sec.php
@@ -5,11 +5,14 @@
 
 	foreach ($mv as $key => $value) {
 		$query = "UPDATE user SET sec='".(int)$_POST['movein']."' WHERE id='".(int)$value."' ";
+
 		$result = mysqli_query($con,$query);
 		if(!$result)
 			die('Error: ' . mysqli_error());
 		else
 			echo "success";
+		//remove status --optional
+		$setStatus=mysqli_query($con,"UPDATE user SET op_status='No Status' WHERE id='".(int)$value."'");
 		
 	}
 mysqli_close($con);
