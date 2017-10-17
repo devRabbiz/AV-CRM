d910ff756f1af37ee740cc11c70ce9f605f63d28
adalenv <adalenvladi@gmail.com>
2017-10-17 10:29:16 +0200

    val to id

--- move_sec.php
+++ move_sec.php
@@ -3,8 +3,8 @@
 	include 'db_connect.php';
 	$mv = json_decode($_POST['mv']);
 
-	foreach ($mv as $key => $value) {
-		$query = "UPDATE user SET sec='".(int)$_POST['movein']."' WHERE id='".(int)$value."' ";
+	foreach ($mv as $key => $id) {
+		$query = "UPDATE user SET sec='".(int)$_POST['movein']."' WHERE id='".(int)$id."' ";
 
 		$result = mysqli_query($con,$query);
 		if(!$result)
@@ -12,7 +12,7 @@
 		else
 			echo "success";
 		//remove status --optional
-		$setStatus=mysqli_query($con,"UPDATE user SET op_status='No Status' WHERE id='".(int)$value."'");
+		$setStatus=mysqli_query($con,"UPDATE user SET op_status='No Status' WHERE id='".(int)$id."'");
 		
 	}
 mysqli_close($con);
