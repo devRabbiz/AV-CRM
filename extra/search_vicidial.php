8a6ab7aa28cd36752a99ccca833e07f8a7d75d95
adalenv <adalenvladi@gmail.com>
2017-12-01 18:58:55 +0100

    search on vicidial

--- extra/search_vicidial.php
+++ extra/search_vicidial.php
@@ -8,10 +8,24 @@ include("../db_connect.php");
 <head>
   <title>CRM</title>
   <meta charset="utf-8">
+    <script type="text/javascript" src="../dist/js/jquery-3.1.1.min.js"></script>
+
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../dist/css/skins/_all-skins.css">
+  <link rel="stylesheet" href="../dist/css/bootstrap-datetimepicker.min.css" />
+
+
+ 
+  <script src="../dist/js/moment.js"></script>
+  <script src="../dist/js/clipboard.min.js"></script>
+  <script type="text/javascript" src="../dist/js/bootstrap-datetimepicker.min.js"></script>
+    <script type="text/javascript" src="../dist/js/jquery.tablesorter.min.js"></script>
+        <script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+        <script src="../dist/js/jquery.slimscroll.min.js"></script>
+   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../dist/css/bootstrapPlusPlus.css" />
+     
+
 </head>
 <style>
 
@@ -64,28 +78,49 @@ input[type=text] {
 
 </style>
 <body>
-<div class="container" style="margin-left: -0px;padding-bottom: 20px;">
+<div class="container" >
 	<form form class="form-wrapper cf" class="form-group" action="" method="POST">
-	<center><h3>Search Database</h3></center>
-		<center><table>
-			<tr>
-				<td><input  placeholder="Search here..." required type="text" name="name" size="30"></td>
-				<td><input class="btn btn-primary" type="submit" name="submit"></td>
-			</tr>
-		</table></center>
+	<center>
+    <h3>Search Vicidial Database</h3>
+        <div class="input-group">
+				<input class="form-control" style="text-align: center;font-size: 16px;float: left;width: 70%" <?php if (isset($_POST['name'])): ?>
+          value="<?php echo $_POST['name'] ?>"
+        <?php endif ?>  placeholder="Search here..." required type="text" name="name" size="30">
+				<input style="float: left;width: 30%"  class="btn btn-primary" type="submit" name="submit">
+        </div>
+		</center>
 	</form>
 </div>
 
 <div>
-
+<br>
  <?php
  if($_REQUEST['submit']){
 	$name = $_POST['name'];
+  $name_ = $_POST['name'];
+  $name=explode(' ',$_POST['name']);
+  $first_name=$name[0];
+  $last_name=$name[1];
+  $len=sizeof($name);
+  if ($len==1) {
+    $sql_query="SELECT * FROM vicidial_list WHERE  (first_name LIKE '" . $name_ . "%' OR last_name LIKE '" . $name_ . "%' OR phone_number LIKE '".$name_."') LIMIT 20 ";
+  }
+  if ($len==2) {
+
+    $sql_query="SELECT * FROM vicidial_list WHERE  (first_name LIKE '" . $first_name . "%' AND last_name LIKE '" . $last_name . "%' ) LIMIT 20 ";
+  }
 	
-	$sql_query="SELECT * FROM vicidial_list WHERE  (first_name LIKE '" . $name . "%' OR last_name LIKE '" . $name . "%' OR phone_number LIKE '".$name."') LIMIT 10 ";
+
 	$result=mysqli_query($dbconfig,$sql_query);
 	echo '<table class="table table-striped table-bordered table-hover">'; 
-  echo"<TR><TD>LeadID</TD><TD>First Name</TD><TD>Last Name</TD><TD>User</TD></TR>";
+
+  echo"<TR>
+      <TD style='width:1px;'>LeadID</TD>
+      <TD >First Name</TD>
+      <TD>Last Name</TD>
+      <td>Phone Number</td>
+      <TD>User</TD>
+    </TR>";
 while($row = mysqli_fetch_array($result))
 {
   echo "<tr><td>"; 
@@ -95,7 +130,9 @@ while($row = mysqli_fetch_array($result))
   echo "</td><td>";    
   echo $row['last_name'];
   echo "</TD><td>";    
-  echo $row['user'];
+  echo $row['phone_number'];
+  echo "</TD><td>";  
+    echo $row['user'];
   echo "</TD></tr>";  
 }
  }
